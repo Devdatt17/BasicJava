d46524057aedae8a61fa58f3eb4dfd7247e45355
Devdatt17 <devdattpandit99@gmail.com>
2020-12-02 19:34:06 +0530

    Tutorial 42 - toString Method

---
+++ toStringMethod.java
@@ -0,0 +1,23 @@
+//* In build java toString method 
+//* Tutorial 42
+public class toStringMethod {
+    private int month;
+    private int day;
+    private int year;
+
+    public toStringMethod(int m, int d, int y) {
+        month = m;
+        day = d;
+        year = y;
+        // ? Returns a string representation of the object
+        // ? In general, the toString method returns a string that "textually
+        // ?represents"
+        // ? this object
+        System.out.printf("The constructor for this is %s\n", this);
+    }
+
+    // * In-build function
+    public String toString() {
+        return String.format("%d/%d/%d", month, day, year);
+    }
+}
---
+++ toStringMethodCPY.java
@@ -0,0 +1,7 @@
+//* In build java toString method 
+//* Tutorial 42
+public class toStringMethodCPY {
+    public static void main(String args[]) {
+        toStringMethod newObject = new toStringMethod(4, 2, 6);
+    }
+}
